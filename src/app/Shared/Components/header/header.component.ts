bc7c99d905c1b1aa4f87de59f0df532fb308eb74
AbdoSakrEgy <abdulrahimsakr01@gmail.com>
2025-04-04 21:30:10 +0200

    add header component

--- src/app/Shared/Components/header/header.component.ts
+++ src/app/Shared/Components/header/header.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { bootstrapSearch } from '@ng-icons/bootstrap-icons';
+import {
+  solarUser,
+  solarSettings,
+  solarBell,
+  solarMoon,
+  solarSun2,
+} from '@ng-icons/solar-icons/outline';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  viewProviders: [
+    provideIcons({
+      bootstrapSearch,
+      solarUser,
+      solarSettings,
+      solarBell,
+      solarMoon,
+      solarSun2,
+    }),
+  ],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isDarkMode = true;
+  changeMode() {
+    this.isDarkMode = !this.isDarkMode;
+  }
+}
