5dfe216eb53c1325925bfa8750d5386a6c9a4755
AbdoSakrEgy <abdulrahimsakr01@gmail.com>
2025-04-06 20:05:18 +0200

    update navbar/update header

--- src/app/Shared/Components/nav-bar/nav-bar.component.ts
+++ src/app/Shared/Components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { bootstrapList, bootstrapPersonVcard } from '@ng-icons/bootstrap-icons';
+import {
+  solarHome,
+  solarUsersGroupRounded,
+  solarDocumentsMinimalistic,
+  solarUser,
+  solarCalendarMinimalistic,
+  solarQuestionCircle,
+  solarChart,
+  solarPieChart,
+  solarCourseUp,
+  solarMap,
+} from '@ng-icons/solar-icons/outline';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [NgIcon, RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrl: './nav-bar.component.css',
+  viewProviders: [
+    provideIcons({
+      bootstrapList,
+      bootstrapPersonVcard,
+      solarHome,
+      solarUsersGroupRounded,
+      solarDocumentsMinimalistic,
+      solarUser,
+      solarCalendarMinimalistic,
+      solarQuestionCircle,
+      solarChart,
+      solarPieChart,
+      solarCourseUp,
+      solarMap,
+    }),
+  ],
 })
-export class NavBarComponent {
-
-}
+export class NavBarComponent {}
