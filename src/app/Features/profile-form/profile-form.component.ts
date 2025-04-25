a956466745229201597895ed72a86f4b850d4689
AbdoSakrEgy <abdulrahimsakr01@gmail.com>
2025-04-26 02:31:19 +0300

    create profileUser page with white inputs

--- src/app/Features/profile-form/profile-form.component.ts
+++ src/app/Features/profile-form/profile-form.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormsModule,
+  FormControl,
+  ReactiveFormsModule,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-profile-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './profile-form.component.html',
-  styleUrl: './profile-form.component.css'
+  styleUrl: './profile-form.component.css',
 })
 export class ProfileFormComponent {
-
+  profileForm = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(30),
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(30),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(11),
+      Validators.maxLength(11),
+    ]),
+    address: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(30),
+    ]),
+  });
+  onSubmit() {
+    console.log(this.profileForm);
+  }
 }
